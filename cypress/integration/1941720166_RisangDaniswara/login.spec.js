b4b7fd1f5048f6fd613a8e3a2b405f3f726eac48
Risang <56747783+Zwarzen@users.noreply.github.com>
2021-12-21 16:40:44 +0700

    rev-test

--- cypress/integration/1941720166_RisangDaniswara/login.spec.js
+++ cypress/integration/1941720166_RisangDaniswara/login.spec.js
@@ -1,4 +1,4 @@
-it('Login Test Normal', () => {
+it('Login Test Berhasil', () => {
     it('Should be Can Login', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('risang22')
@@ -8,62 +8,57 @@ it('Login Test Normal', () => {
     })
 })
 
-it('Login Test Normal', () => {
+it('Login Test Password Salah', () => {
     it('Password Salah', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('risang22')
     cy.get('#password').type('aniswara123')
     cy.get('#submit').click()
 
-    //cy.get('#react-burger-menu-btn').click()
-    //cy.get('#logout_sidebar_link').click()
+    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
     })
 })
 
-it('Login Test Normal', () => {
+it('Login Test Username Salah', () => {
     it('Username Salah', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('isang22')
     cy.get('#password').type('Daniswara123')
     cy.get('#submit').click()
 
-    //cy.get('#react-burger-menu-btn').click()
-    //cy.get('#logout_sidebar_link').click()
+    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
     })
 })
 
-it('Login Test Normal', () => {
+it('Login Test Username Kosong', () => {
     it('Username Kosong', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('')
     cy.get('#password').type('Daniswara123')
     cy.get('#submit').click()
 
-    //cy.get('#react-burger-menu-btn').click()
-    //cy.get('#logout_sidebar_link').click()
+    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
     })
 })
 
-it('Login Test Normal', () => {
+it('Login Test Password Kosong', () => {
     it('Password kosong', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('risang22')
     cy.get('#password').type('')
     cy.get('#submit').click()
 
-    //cy.get('#react-burger-menu-btn').click()
-    //cy.get('#logout_sidebar_link').click()
+    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
     })
 })
 
-it('Login Test Normal', () => {
+it('Login Test Username dan Password Kosong', () => {
     it('USername dan Password Kosong', () => {
     cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
     cy.get('#username').type('')
     cy.get('#password').type('')
     cy.get('#submit').click()
 
-    //cy.get('#react-burger-menu-btn').click()
-    //cy.get('#logout_sidebar_link').click()
+    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
     })
 })
