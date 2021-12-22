2f391e2da74474f9239c4569c364431e5105bd9c
Risang <56747783+Zwarzen@users.noreply.github.com>
2021-12-22 10:41:33 +0700

    rev-test

--- cypress/integration/1941720166_RisangDaniswara/login.spec.js
+++ cypress/integration/1941720166_RisangDaniswara/login.spec.js
@@ -1,64 +1,59 @@
-it('Login Test Berhasil', () => {
-    it('Should be Can Login', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('risang22')
-    cy.get('#password').type('Daniswara123')
-    cy.get('#submit').click()
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+describe('Test to make submission', () => {
+    it('Login Test Berhasil', () => {
+       
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type('risang22')
+        cy.get('#password').type('Daniswara123')
+        cy.get('#login > fieldset > div.buttons > button').click()
+        cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+        
     })
-})
 
-it('Login Test Password Salah', () => {
-    it('Password Salah', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('risang22')
-    cy.get('#password').type('aniswara123')
-    cy.get('#submit').click()
+    it('Login Test Password Salah', () => {
+        
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type('risang22')
+        cy.get('#password').type('aniswara123')
+        cy.get('#login > fieldset > div.buttons > button').click()
 
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+        
     })
-})
 
-it('Login Test Username Salah', () => {
-    it('Username Salah', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('isang22')
-    cy.get('#password').type('Daniswara123')
-    cy.get('#submit').click()
-
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+    it('Login Test Username Salah', () => {
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type('isang22')
+        cy.get('#password').type('Daniswara123')
+        cy.get('#login > fieldset > div.buttons > button').click()
+        
     })
-})
+    
+    it('Login Test Username Kosong', () => {
+        
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type(' ')
+        cy.get('#password').type('Daniswara123')
+        cy.get('#login > fieldset > div.buttons > button').click()
 
-it('Login Test Username Kosong', () => {
-    it('Username Kosong', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('')
-    cy.get('#password').type('Daniswara123')
-    cy.get('#submit').click()
-
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+        
     })
-})
 
-it('Login Test Password Kosong', () => {
-    it('Password kosong', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('risang22')
-    cy.get('#password').type('')
-    cy.get('#submit').click()
+    it('Login Test Password Kosong', () => {
+        
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type('risang22')
+        cy.get('#password').type(' ')
+        cy.get('#login > fieldset > div.buttons > button').click()
 
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+        
     })
-})
 
-it('Login Test Username dan Password Kosong', () => {
-    it('USername dan Password Kosong', () => {
-    cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
-    cy.get('#username').type('')
-    cy.get('#password').type('')
-    cy.get('#submit').click()
+    it('Login Test Username dan Password Kosong', () => {
+        
+        cy.visit('http://jip.polinema.ac.id/ojs3/index.php/jip/login')
+        cy.get('#username').type(' ')
+        cy.get('#password').type(' ')
+        cy.get('#login > fieldset > div.buttons > button').click()
 
-    cy.get('#navigationUser > li:nth-child(1) > a').contains('Logout')
+        
     })
 })
